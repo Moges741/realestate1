5d38e14eac70aa1a87d25885b675deaa43d9df38
Moges <mogesse741@gmial.com>
2026-01-02 10:53:45 +0300

    added backend and connected db

---
+++ server/index.js
@@ -0,0 +1,10 @@
+
+import './DB/connectDB.js'; 
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} : http://localhost:${PORT}`);
+});
